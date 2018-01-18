6c0564a52a89ab86dcb87da3b743636147a11d1d
heron.sanches <heron.sanches@eteg.com.br>
2018-01-17 23:33:46 -0200

    tfragments-01-exercise

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -11,11 +11,16 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.List;
+
 /**
  * Created by Heron Sanches on 2018-01-16.
  */
 
 public class BodyPartFragment extends Fragment {
+
+    private List<Integer> imageIds;
+    private List<Integer> indexImageIds;
 
 
     @Nullable
@@ -30,4 +35,13 @@ public class BodyPartFragment extends Fragment {
     }
 
 
+    public void setImageIds(List<Integer> imageIds) {
+        this.imageIds = imageIds;
+    }
+
+    public void setIndexImageIds(List<Integer> indexImageIds) {
+        this.indexImageIds = indexImageIds;
+    }
+
+    
 }
